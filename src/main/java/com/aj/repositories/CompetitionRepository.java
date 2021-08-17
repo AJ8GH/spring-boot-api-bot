0b0fff70789b2187f64d519f634ec4235b760119
Adam Jonas <adam.jonas@ppb.com>
2021-08-17 09:31:25 +0100

    Remove competition model

--- src/main/java/com/aj/repositories/CompetitionRepository.java
+++ src/main/java/com/aj/repositories/CompetitionRepository.java
@@ -1,6 +1,5 @@
 package com.aj.repositories;
 
-import com.aj.models.Competition;
 import org.springframework.data.repository.CrudRepository;
 
 public interface CompetitionRepository extends CrudRepository<Competition, Long> {
