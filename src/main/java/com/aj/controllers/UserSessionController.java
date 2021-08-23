fb331f2f1c539c0eb512b451038ed993560fc500
Adam Jonas <adam.jonas@ppb.com>
2021-08-23 17:58:11 +0100

    Refactor session routes into constants

--- src/main/java/com/aj/controllers/UserSessionController.java
+++ src/main/java/com/aj/controllers/UserSessionController.java
@@ -13,28 +13,35 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 @AllArgsConstructor
 public class UserSessionController extends AbstractController {
+    private final String REDIRECT = "redirect:";
+    private final String LOGIN = "/login";
+    private final String SESSIONS_NEW = "/sessions/new";
+    private final String INDEX_ROUTE = "/";
+    private final String INDEX_VIEW = "index";
+
     private final ApiClientService apiClient;
     private final DeserialisationService jsonDeserialiser;
 
-    @RequestMapping("/")
+    @RequestMapping(INDEX_ROUTE)
     public String getIndex() {
-        if (isNotLoggedIn(apiClient.getUserSession())) return "redirect:/login";
-        return "index";
+        if (isNotLoggedIn(apiClient.getUserSession())) return REDIRECT + LOGIN;
+        return INDEX_VIEW;
     }
 
-    @RequestMapping("/login")
+    @RequestMapping(LOGIN)
     public String login(Model model) {
         model.addAttribute("userSession", apiClient.getUserSession());
-        return "login";
+        return LOGIN;
     }
 
-    @PostMapping("/sessions/new")
+    @PostMapping(SESSIONS_NEW)
     public String login(@RequestParam("username") String username,
                         @RequestParam("password") String password)
             throws Exception {
+
         String response = apiClient.login(username, password);
         UserSession userSession = jsonDeserialiser.mapToObject(response, UserSession.class);
         apiClient.setUserSession(userSession);
-        return "redirect:/";
+        return REDIRECT + INDEX_ROUTE;
     }
 }
